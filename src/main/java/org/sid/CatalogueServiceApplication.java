2b05ef8b555d092956d9e0f245cdf0f81929595d
jeremie <alcaraz.jeremie@hotmail.fr>
2019-07-28 01:06:10 +0200

    add category in memory -> App ok

--- src/main/java/org/sid/CatalogueServiceApplication.java
+++ src/main/java/org/sid/CatalogueServiceApplication.java
@@ -1,13 +1,32 @@
 package org.sid;
 
+import org.sid.dao.CategoryRepository;
+import org.sid.dao.ProductRepository;
+import org.sid.entities.Category;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class CatalogueServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CatalogueServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(CategoryRepository categoryRepository, ProductRepository productRepository){
+        return args->{
+            categoryRepository.deleteAll();
+            Stream.of("Ordinateurs", "Imprimantes").forEach(c->{
+                categoryRepository.save(new Category("C1", c, new ArrayList<>()));
+            });
+            categoryRepository.findAll().forEach(System.out::println);
+        };
     }
 
 }
